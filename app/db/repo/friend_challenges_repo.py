b84375bf17c824143e6d3ada5f625d1d70caf209
Valerii S <svalerii535@gmail.com>
2026-04-22 20:15:33 +0200

    refactor(repo): split friend challenges repo (#141)
    Co-authored-by: Quiz Arena <local@quiz-arena>

--- app/db/repo/friend_challenges_repo.py
+++ app/db/repo/friend_challenges_repo.py
@@ -1,41 +1,8 @@
 from __future__ import annotations
 
-from app.db.repo.friend_challenges_repo_access import (
-    create,
-    get_by_id,
-    get_by_id_for_update,
-    get_by_invite_token,
-    get_by_invite_token_for_update,
-    list_by_series_id_for_update,
-    list_recent_for_user,
-)
-from app.db.repo.friend_challenges_repo_metrics import (
-    count_by_creator_access_type,
-    count_created_since,
-    count_live_for_user,
-    count_live_open_by_creator,
-    list_active_due_for_expire_for_update,
-    list_active_due_for_last_chance_for_update,
-    list_joined_due_for_walkover_for_update,
-    list_pending_due_for_expire_for_update,
-)
+from app.db.repo.friend_challenges_repo_core import FriendChallengesRepoCoreMixin
+from app.db.repo.friend_challenges_repo_deadlines import FriendChallengesRepoDeadlineMixin
 
 
-class FriendChallengesRepo:
-    get_by_id = staticmethod(get_by_id)
-    get_by_id_for_update = staticmethod(get_by_id_for_update)
-    get_by_invite_token = staticmethod(get_by_invite_token)
-    get_by_invite_token_for_update = staticmethod(get_by_invite_token_for_update)
-    create = staticmethod(create)
-    count_by_creator_access_type = staticmethod(count_by_creator_access_type)
-    count_live_for_user = staticmethod(count_live_for_user)
-    count_live_open_by_creator = staticmethod(count_live_open_by_creator)
-    count_created_since = staticmethod(count_created_since)
-    list_recent_for_user = staticmethod(list_recent_for_user)
-    list_active_due_for_last_chance_for_update = staticmethod(
-        list_active_due_for_last_chance_for_update
-    )
-    list_active_due_for_expire_for_update = staticmethod(list_active_due_for_expire_for_update)
-    list_pending_due_for_expire_for_update = staticmethod(list_pending_due_for_expire_for_update)
-    list_joined_due_for_walkover_for_update = staticmethod(list_joined_due_for_walkover_for_update)
-    list_by_series_id_for_update = staticmethod(list_by_series_id_for_update)
+class FriendChallengesRepo(FriendChallengesRepoCoreMixin, FriendChallengesRepoDeadlineMixin):
+    pass
